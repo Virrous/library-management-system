8b07e2ebe60267a8cdc50feb2d018bbbaaf76273
Ashes Pokhrel <123297869+Virrous@users.noreply.github.com>
2024-06-23 01:29:33 +0545

    final backend

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
-from django.db import models.
+from django.db import models
+
 class Book(models.Model):
     FICTION = 'fiction'
     NOVELS = 'nobels'
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render,redirect,HttpResponse
+from email.utils import parsedate
+from django.shortcuts import render,redirect,HttpResponse,get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
 from .models import *
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -34,7 +38,7 @@ def form(request):
                 login(request,user)
                 return redirect('dashboard')
             else:
-                return HttpResponse ('incorrect!!')
+                return redirect('form')
     return render(request, 'form.html')
 
 # def login_view(request):
@@ -62,23 +66,63 @@ def dashboard(request):
     return render(request, 'dashboard.html',{'books':books,'students':students, 'issues':issues})
 
 def issueBook(request):
-    issues = Issued_Book.objects.all()
-    return render(request, 'issueBook.html',{'issues':issues})
-
-def books(request):
-    
-    if request.method == 'POST':
-       title=request.POST.get('title')
-       author=request.POST.get('author')
-       type = request.POST.get('types')
-       shelf=request.POST.get('shelf')
-       quantity=request.POST.get('quantity')
-    #    print(title,author,type,shelf,quantity)
-       book_item=Book(title=title,author=author,type=type,shelf=shelf,quantity=quantity)
-       book_item.save()
-        
+    if request.method=="POST":
+        if 'add_another' in request.POST:
+            book_id = request.POST.get('book_id')
+            student_id = request.POST.get('student_id') 
+            issue_date = request.POST.get('issue_date')
+            return_date = request.POST.get('return_date')
+            issue_date = parsedate(issue_date)
+            book = Book.objects.get(id=book_id)
+            student = Student.objects.get(id=student_id)
+            issued_book = Issued_Book(book=book, student=student,issue_date=issue_date,return_date=return_date)
+            # print(issued_book)
+            issued_book.save()
+            book.quantity -=1
+            book.save()
+            
+        elif 'delete_issue' in request.POST:
+                student_id = request.POST.get('student_id')
+                student = get_object_or_404(Student, id=student_id)
+                student.delete()
+        elif 'query' in request.POST:
+                query = request.POST.get('query')
+                issues = Issued_Book.objects.filter(book__title__icontains=query) | Issued_Book.objects.filter(student__name__icontains=query)
+                books = Book.objects.all()
+                students = Student.objects.all()
+                return render(request, 'issueBook.html',{'issues':issues,'books':books,'students':students})
+            
     books = Book.objects.all()
-    return render(request, 'books.html', {'books': books,'form': form})
+    students = Student.objects.all()
+    issues = Issued_Book.objects.all()
+    return render(request, 'issueBook.html',{'issues':issues,'books':books,'students':students})
+
+# @csrf_exempt
+def books(request):
+    if request.method == 'POST':
+            # Handle form submissions for adding/searching books
+            if 'add_another' in request.POST:
+                title = request.POST.get('title')
+                author = request.POST.get('author')
+                type = request.POST.get('types')
+                shelf = request.POST.get('shelf')
+                quantity = request.POST.get('quantity')
+                book_item = Book(title=title, author=author, type=type, shelf=shelf, quantity=quantity)
+                book_item.save()
+                
+            elif 'delete_book' in request.POST:
+                book_id = request.POST.get('book_id')
+                book = get_object_or_404(Book, id=book_id)
+                book.delete()
+            
+            elif 'query' in request.POST:
+                query = request.POST.get('query')
+                books = Book.objects.filter(title__icontains=query)
+                return render(request, 'books.html', {'books': books})
+
+    books = Book.objects.all()
+    return render(request, 'books.html', {'books': books})
+
 
 def add_book(request):
    pass
@@ -86,14 +130,25 @@ def add_book(request):
 def students(request):
     try:
         if request.method == 'POST':
-            name1 = request.POST.get('name')
-            address1 = request.POST.get('address')
-            contact1 = request.POST.get('contact')
-            email1 = request.POST.get('email')
-            # print(name1,address1,contact1,email1)
-            student_item = Student(name=name1, address=address1,email=email1,contact=contact1)
-            # print(student_item)
-            student_item.save()
+            if 'add_another' in request.POST:
+                name1 = request.POST.get('name')
+                address1 = request.POST.get('address')
+                contact1 = request.POST.get('contact')
+                email1 = request.POST.get('email')
+                # print(name1,address1,contact1,email1)
+                student_item = Student(name=name1, address=address1,email=email1,contact=contact1)
+                # print(student_item)
+                student_item.save()
+                
+            elif 'delete_student' in request.POST:
+                student_id = request.POST.get('student_id')
+                student = get_object_or_404(Student, id=student_id)
+                student.delete()
+                
+            elif 'query' in request.POST:
+                query = request.POST.get('query')
+                students = Student.objects.filter(name__icontains=query)
+                return render(request, 'student.html', {'students': students})
     except:
         pass
         
